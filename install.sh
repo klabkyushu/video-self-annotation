00cdc674a141ae339a7a1bcc655527239627f5ba
r0bertr <dr.paper@live.com>
2020-10-17 04:38:28 +0000

    Add install.sh, requirements.txt for dependencies installation.

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+project_root=$(pwd)
+
+pip install -r requirements.txt
+
+# cocoapi
+cd $project_root/vendors/cocoapi/PythonAPI
+python setup.py build_ext install
+
+# apex
+cd $project_root/vendors/apex
+pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" .
+
+# video-maskrcnn
+cd $project_root/vendors/video-maskrcnn
+python setup.py build develop
+
+# Download pre-trained models
+# https://drive.google.com/file/d/10bqv7fUeUEdT1Q9T617QTcttit5EJi76/view
+mkdir -p $project_root/annotation
+cd $project_root/annotation
+mkdir -p CityScapes/val/Dataset/Images
+mkdir -p CityScapes/val/Raw
+cd CityScapes/val/
+fileid="10bqv7fUeUEdT1Q9T617QTcttit5EJi76"
+filename="Initial_model.zip"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+rm ./cookie
+unzip Initial_model.zip
+rm Initial_model.zip
+
+# CIResNet22_RPN.pth: https://drive.google.com/file/d/1tBllNtv-90Ih2EP_lnRCBzxnZeFPRnPx/view?usp=sharing
+cd $project_root/vendors/video-maskrcnn/maskrcnn_benchmark/external/tracking/SOT/SiamDW/
+mkdir -p snapshot && cd snapshot
+fileid="1tBllNtv-90Ih2EP_lnRCBzxnZeFPRnPx"
+filename="CIResNet22_RPN.pth"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+rm ./cookie
+
+cd $project_root
